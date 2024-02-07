128bac67ae6a04073684d5b8ece99f44da08f0ed
Mister-Me <Hs.Soleymani7@gmail.com>
2024-02-07 15:07:24 +0330

    class path modified

--- include/Path.hpp
+++ include/Path.hpp
@@ -7,18 +7,15 @@
 class Path
 {
 public:
-    void setTrainDistance(unsigned int);             /* sets the distance between two stations connected by train */
-    void setTaxiDistance(unsigned int);              /* sets the distance between two stations connected by taxi  */
+    void setTrainTaxiDistance(unsigned int);         /* sets the distance between two stations connected by train or taxi */
     void setBusDistance(unsigned int);               /* sets the distance between two stations connected by bus   */
-    unsigned int getTrainDistance() const;           /* returns the distance between two stations connected by train */
-    unsigned int getTaxiDistance() const;            /* returns the distance between two stations connected by taxi  */
+    unsigned int getTrainTaxiDistance() const;       /* returns the distance between two stations connected by train or taxi */
     unsigned int getBusDistance() const;             /* returns the distance between two stations connected by bus   */
     void setDijkstraList(std::string);               /* pushes a station into a list which is used for dijkstra algorithm */ 
     void clearDijkstraList();                        /* clears the list if it updates  */
     void printDijkstraList();                        /* prints the shortest path given from dijkstra algorithm  */
 private:
-    unsigned int trainDistance;                      /* distance between two stations connected by train */
-    unsigned int taxiDistance;                       /* distance between two stations connected by taxi  */
+    unsigned int trainTaxiDistance;                  /* distance between two stations connected by train */
     unsigned int busDistance;                        /* distance between two stations connected by bus   */
     std::vector<std::string> dijkstraList;           /* list for showing the stations after dijkstra algorithm */
 };
--- src/Path.cpp
+++ src/Path.cpp
@@ -1,54 +1,49 @@
 #include <iostream>
 #include "Path.hpp"
 
-// sets the distance between two stations connected by train 
-void Path::setTrainDistance(unsigned int distance) {
-    this->trainDistance = distance;
+// sets the distance between two stations connected by train or taxi
+void Path::setTrainTaxiDistance(unsigned int distance) 
+{
+    this->trainTaxiDistance = distance;
 }     
 
 
-// sets the distance between two stations connected by taxi 
-void Path::setTaxiDistance(unsigned int distance) {
-    this->taxiDistance = distance;
-} 
-
-
 // sets the distance between two stations connected by bus 
-void Path::setBusDistance(unsigned int distance) {
+void Path::setBusDistance(unsigned int distance) 
+{
     this->busDistance = distance;
 }  
 
 
-// returns the distance between two stations connected by train 
-unsigned int Path::getTrainDistance() const {
-    return (this->trainDistance);
-}
-
-
-// returns the distance between two stations connected by taxi 
-unsigned int Path::getTaxiDistance() const {
-    return (this->taxiDistance);
+// returns the distance between two stations connected by train ot taxi
+unsigned int Path::getTrainTaxiDistance() const 
+{
+    return (this->trainTaxiDistance);
 }
 
 
 // returns the distance between two stations connected by bus 
-unsigned int Path::getBusDistance() const {
+unsigned int Path::getBusDistance() const 
+{
     return (this->busDistance);
 }    
 
 // pushes a station into a list which is used for dijkstra algorithm 
-void Path::setDijkstraList(std::string station) {
+void Path::setDijkstraList(std::string station) 
+{
     this->dijkstraList.push_back(station);
 }
 
 
 // clears the list if it updates 
-void Path::clearDijkstraList() {
+void Path::clearDijkstraList() 
+{
     this->dijkstraList.clear();
 }                     
 
 // prints the shortest path given from dijkstra algorithm 
-void Path::printDijkstraList() {
+void Path::printDijkstraList() 
+{
     for (auto i : dijkstraList)
         std::cout << i << " ";
 }                      
